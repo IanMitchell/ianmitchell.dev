23e9d97fa1456b40881af41eb1138c1defc9c9bb
Ian Mitchell <ian.mitchel1@live.com>
2020-08-11 12:33:22 -0700

    Feedbin Post

--- pages/_app.js
+++ pages/_app.js
@@ -53,6 +53,11 @@ export default function App({ Component, pageProps }) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 120 100%22><text y=%22.9em%22 font-size=%2290%22>🤓</text></svg>"
         />
+        <link
+          rel="mask-icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 120 100%22><text y=%22.9em%22 font-size=%2290%22>🤓</text></svg>"
+          color="#333333"
+        />
 
         <link
           rel="preconnect"
