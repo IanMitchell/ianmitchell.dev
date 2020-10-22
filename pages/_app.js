89ea00b84b9eab381a49cf6c71f0d4c1a4774f12
Ian Mitchell <ian.mitchell@hey.com>
2020-10-22 13:21:16 -0700

    Update Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from "react";
-import Head from "next/head";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as Fathom from "fathom-client";
-import { SWRConfig } from "swr";
-import Font from "../components/Font";
-import MDX from "../components/MDX";
-import Social from "../components/Social";
-import { logMetric } from "../lib/metrics";
-import "../styles/main.scss";
+import React, { Fragment } from 'react';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+import { SWRConfig } from 'swr';
+import Font from '../components/Font';
+import MDX from '../components/MDX';
+import Social from '../components/Social';
+import { logMetric } from '../lib/metrics';
+import '../styles/main.scss';
 
 export function reportWebVitals(metric) {
   // Log our top five metrics
   switch (metric.name) {
-    case "FCP": // First Contentful Paint
-    case "LCP": // Largest Contentful Paint
-    case "FID": // First Input Delay
-    case "CLS": // Cumulative Layout Shift
-    case "TTFB": // Time to First Byte
+    case 'FCP': // First Contentful Paint
+    case 'LCP': // Largest Contentful Paint
+    case 'FID': // First Input Delay
+    case 'CLS': // Cumulative Layout Shift
+    case 'TTFB': // Time to First Byte
       logMetric(metric);
       break;
   }
@@ -28,20 +28,20 @@ export default function App({ Component, pageProps }) {
 
   useEffect(() => {
     // Initialize Fathom when the app loads
-    Fathom.load("LKSXEXQK", {
-      includedDomains: ["ianmitchell.dev"],
-      url: "https://macaw.ianmitchell.dev/script.js",
+    Fathom.load('LKSXEXQK', {
+      includedDomains: ['ianmitchell.dev'],
+      url: 'https://macaw.ianmitchell.dev/script.js',
     });
 
     function onRouteChangeComplete() {
       Fathom.trackPageview();
     }
     // Record a pageview when route changes
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
 
     // Unassign event listener
     return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
     };
   }, [router]);
 
@@ -51,12 +51,8 @@ export default function App({ Component, pageProps }) {
         <title>Ian Mitchell</title>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 120 100%22><text y=%22.9em%22 font-size=%2290%22>🤓</text></svg>"
-        />
-        <link
-          rel="mask-icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 120 100%22><text y=%22.9em%22 font-size=%2290%22>🤓</text></svg>"
-          color="#333333"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22%231e2235%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2270%22>🤓</text></svg>"
         />
 
         <link
