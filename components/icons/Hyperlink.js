c45c20153af0434fff1ed2e1272d8a73b16cae72
Ian Mitchell <ian.mitchell@hey.com>
2021-04-28 14:45:42 -0700

    Icon Fixes

--- components/icons/Hyperlink.js
+++ components/icons/Hyperlink.js
@@ -12,7 +12,6 @@ export default function Hyperlink() {
       shapeRendering="geometricPrecision"
       role="img"
     >
-      <title>Link icon</title>
       <path d="M10 13a5 5 0 007.54.54l3-3a5 5 0 00-7.07-7.07l-1.72 1.71"></path>
       <path d="M14 11a5 5 0 00-7.54-.54l-3 3a5 5 0 007.07 7.07l1.71-1.71"></path>
     </svg>
