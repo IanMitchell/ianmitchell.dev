3e62964cd2e6d2df10d4a103e1e85e5891a71cdf
Ian Mitchell <ian.mitchell@hey.com>
2022-02-11 22:44:49 -0800

    Add Sponsor Webhook

--- pages/api/webhooks/sponsors.js
+++ pages/api/webhooks/sponsors.js
@@ -1,36 +1,75 @@
-export default function () {}
-// export default function SponsorsWebhook(request, response) {
-// if (request.method !== "POST") {
-//   return;
-// }
-// const payload = request.body;
-// const embed = {
-//   footer: {
-//     icon_url: "",
-//     text: "GitHub Sponsors",
-//   },
-//   timestamp: payload?.sponsorship?.created_at,
-// };
-// switch (payload.action) {
-//   case "created":
-//     embed.color = 0x65280;
-//     break;
-//   case "pending_tier_change":
-//   default:
-//     embed.color = "";
-// }
-// if (payload?.sponsorship?.privacy_level === "public") {
-//   data.author = {
-//     name: payload?.sponsorship?.sponsor?.login,
-//     url: payload?.sponsorship?.sponsor?.url,
-//     icon_url: payload?.sponsorship?.sponsor?.avatar_url,
-//   };
-// }
-// if (payload?.sponsorship?.tier?.is_one_time) {
-// }
-// if (payload?.sponsorship?.tier?.is_custom_amount) {
-// }
-// if (payload?.sponsorship?.changes?.tier?.from) {
-// }
-// response.status(200);
-// }
+import { MessageEmbed } from "discord.js";
+
+const COLORS = {
+  ADDED: 0x5763719,
+  CHANGED: 0x16705372,
+};
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+export default async function SponsorsWebhook(request, response) {
+  if (request.method !== "POST") {
+    return;
+  }
+
+  const payload = request.body;
+  const embed = new MessageEmbed();
+  const user = payload.sender;
+  const isPublic = payload.sponsorship.privacy_level !== "public";
+  const { tier } = payload.sponsorship;
+  const username = isPublic ? user.log : "Anonymous";
+
+  if (payload.sponsorship.privacy_level === "public") {
+    embed.setAuthor({
+      name: user.login,
+      iconURL: user.avatar_url,
+      url: user.html_url,
+    });
+  }
+
+  embed.setTimestamp(payload.sponsorship.created_at);
+
+  if (payload.action === "created") {
+    embed.setTitle("New Sponsorship");
+    embed.setColor(COLORS.ADDED);
+    embed.setDescription(
+      `${username} just sponsored you for **${formatter.format(
+        tier.monthly_price_in_dollars
+      )}**`
+    );
+    embed.addField("Tier", payload.sponsorship.tier.name ?? "None", true);
+    embed.addField(
+      "One Time Payment?",
+      tier.one_time_payment ? "Yes" : "No",
+      true
+    );
+  } else {
+    embed.setTitle("Sponsorship Tier Change");
+    embed.setColor(COLORS.CHANGED);
+    embed.setDescription(
+      `${username} changed to **${formatter.format(
+        payload.changes.tier.from.monthly_price_in_dollars
+      )}** from ${formatter.format(tier.monthly_price_in_dollars)}`
+    );
+    embed.addField(
+      "One Time Payment?",
+      tier.one_time_payment ? "Yes" : "No",
+      true
+    );
+  }
+
+  const resp = await fetch(process.env.SPONSOR_WEBHOOK, {
+    method: "POST",
+    body: JSON.stringify({ embeds: [embed.toJSON()] }),
+  });
+  const json = await resp.json();
+
+  if (!resp.ok) {
+    throw new Error(JSON.stringify(json));
+  }
+
+  response.status(200);
+}
