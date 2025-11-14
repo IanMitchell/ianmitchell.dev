d91a262602550d9a58f73d25327739d005f717f5
Ian Mitchell <ian.mitchell@hey.com>
2025-11-13 19:56:25 -0800

    Redesign the blog list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,6 @@ import { H1 } from "@/components/md/Heading";
 import { getAllPosts, getPost } from "@/lib/blog-posts";
 import { getSlug } from "@/lib/slug";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Blog",
@@ -40,7 +39,7 @@ export default async function BlogIndexPage() {
 		<Page
 			header={
 				<header className="mt-8 mb-12 flex items-center justify-between">
-					<H1 className="mt-0">Blog Posts</H1>
+					<H1 className="mt-0 w-fit">Blog Posts</H1>
 					<Anchor href="/feed" className="text-sm">
 						RSS Feed
 					</Anchor>
@@ -51,29 +50,29 @@ export default async function BlogIndexPage() {
 				.reverse()
 				.map(([year, posts]) => (
 					<div className="mt-4" key={year}>
-						<h2 className="mb-5">{year}</h2>
-						<ul className="mb-16">
+						<h2 className="mb-4">{year}</h2>
+						<ul className="mb-12 flex flex-col gap-2">
 							{posts
 								.sort((a, b) => b.date.getTime() - a.date.getTime())
 								.map((post) => (
 									<li
 										key={post.title}
-										className="my-4 flex flex-row items-center"
+										className="gap-4 flex flex-row items-center -ml-13"
 									>
-										<Link
-											href={`/blog/${getSlug(post.title)}`}
-											className="border-highlight/30 border-b-2 border-solid text-highlight hover:border-highlight flex items-center gap-2"
-										>
-											{post.title}
-										</Link>
 										<span className="hidden text-xs opacity-80 sm:inline">
-											<span className="mx-2">&bull;</span>
 											{new Date(post.date).toLocaleString("en-US", {
-												month: "numeric",
-												day: "numeric",
+												month: "2-digit",
+												day: "2-digit",
 												timeZone: "UTC",
 											})}
 										</span>
+
+										<Anchor
+											href={`/blog/${getSlug(post.title)}`}
+											className="text-sm"
+										>
+											{post.title}
+										</Anchor>
 									</li>
 								))}
 						</ul>
