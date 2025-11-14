35b1f5c0c37a2c010cde3db7c00eb5319563c919
Ian Mitchell <ian.mitchell@hey.com>
2025-11-14 15:25:40 -0800

    Fixes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
 	return (
-		<div className="border-[3px] p-1 border-[#333] hover:border-highlight w-12 h-12">
+		<div className="border-[3px] p-1 border-[#333] hover:border-brand-blue w-12 h-12">
 			<div className="bg-[#333] h-6 w-6 flex items-center justify-center text-center">
 				<span className="text-xs text-white font-mono">i</span>
 			</div>
--- components/md/BlockQuote.tsx
+++ components/md/BlockQuote.tsx
@@ -2,6 +2,9 @@ import type { ComponentProps } from "react";
 
 export function BlockQuote(props: ComponentProps<"blockquote">) {
 	return (
-		<blockquote {...props} className="border-black mb-4 border-l-4 pl-4" />
+		<blockquote
+			{...props}
+			className="border-black mb-4 border-l-4 pl-4 ml-[2ch]"
+		/>
 	);
 }
